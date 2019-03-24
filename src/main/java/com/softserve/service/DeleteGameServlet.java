f509b55f5fab568eb63ed36fc54e120041613fd7
Roman Boiarchuk <r.s.boyarchuk@gmail.com>
2019-03-24 14:12:59 +0200

    added image remove after game delete

--- src/main/java/com/softserve/service/DeleteGameServlet.java
+++ src/main/java/com/softserve/service/DeleteGameServlet.java
@@ -1,6 +1,7 @@
 package com.softserve.service;
 
 import com.softserve.dao.GamesDao;
+import com.softserve.models.Game;
 import com.softserve.models.User;
 import com.softserve.models.UserRole;
 import com.softserve.utilities.DataBaseUtilities;
@@ -10,6 +11,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.File;
 import java.io.IOException;
 
 @WebServlet(name = "deleteGame", urlPatterns = "/deleteGame")
@@ -24,6 +26,9 @@ public class DeleteGameServlet extends HttpServlet {
         } else if (user.getRole() == UserRole.ADMIN) {
             GamesDao dao = DataBaseUtilities.getGamesDao();
             long gameId = Long.valueOf(req.getParameter("gameId"));
+            Game game = dao.getById(gameId);
+            File image = new File("storage/gamesAvatars/" + game.getImg());
+            image.delete();
             dao.deleteById(gameId);
             String beforeEdit = req.getParameter("previous");
             resp.sendRedirect(beforeEdit);
